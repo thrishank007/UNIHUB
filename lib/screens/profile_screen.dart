74c4ef5e56919d9d0e72ffaabefd3682bf4fbe0e
Jyothish Kumar <darshans1013@gmail.com>
2025-12-12 17:04:35 +0530

    Profile_screen init

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatefulWidget{
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
@@ -15,29 +15,144 @@ class _ProfileScreenState extends State<ProfileScreen> {
         leading: BackButton(
           color: Colors.white,
         ),
-        title: const Text('Profile', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+        title: const Text(
+          'Profile',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(10, 2, 46, 1),
       ),
       body: Stack(
         children: [
-          Positioned.fill(child: Image.asset('assets/images/agent_home.jpeg', fit: BoxFit.cover,)),
+          Positioned.fill(
+              child: Image.asset(
+            'assets/images/agent_home.jpeg',
+            fit: BoxFit.cover,
+          )),
           Column(
             children: [
               Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Container(
-                  height: 120,
-                  width: double.infinity,
                   decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/profile_img.jpeg'),
+                        fit: BoxFit.cover),
                     borderRadius: BorderRadius.circular(12.0),
-                    color: const Color.fromARGB(205, 39, 16, 152)
                   ),
-                  child: Image.asset(''),
+                  child: SizedBox(
+                    height: 250,
+                    width: double.infinity,
+                  ),
                 ),
-              )
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: const Color.fromARGB(237, 88, 87, 106)),
+                  child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 20.0,
+                          ),
+                          const SizedBox(
+                            width: 8.0,
+                          ),
+                          const Text(
+                            'John Doe',
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.0),
+                      color: const Color.fromARGB(237, 88, 87, 106)),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Icon(
+                              Icons.email,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 8.0,
+                            ),
+                            const Text(
+                              'john.doe@example.com',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 8.0,
+                            ),
+                            const Text(
+                              '+1 234 567 890',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 8.0,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.account_box,
+                              color: Colors.white,
+                            ),
+                            const SizedBox(
+                              width: 8.0,
+                            ),
+                            const Text(
+                              'Switch Accounts',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
